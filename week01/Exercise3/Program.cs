ab0c5ba37f66ef396025dfdae6e2d9405f42323b
Paper-Dad <broberts@byupathway.edu>
2025-09-01 18:53:32 -0600

    Completed week1 exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            string attempt = Console.ReadLine();
+            guess = int.Parse(attempt);
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        
     }
 }
