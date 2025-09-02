526e829af7e65a9a3a4e0beb94112236940f09ed
Paper-Dad <broberts@byupathway.edu>
2025-09-01 19:41:33 -0600

    Completed week1 exercise2

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,39 @@
-using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int sum = 0;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter a number: ");
+        string attempt = Console.ReadLine();
+        int number = int.Parse(attempt);
+        while (number != 0)
+        {
+            numbers.Add(number);
+            Console.Write("Enter a number: ");
+            attempt = Console.ReadLine();
+            number = int.Parse(attempt);
+        }
+        foreach (int i in numbers)
+        {
+            sum = i + sum;
+        }
+        float average = ((float)sum) / numbers.Count;
+        int largest = 0;
+        foreach (int j in numbers)
+        {
+            if (j > largest)
+            {
+                largest = j;
+            }
+        }
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
