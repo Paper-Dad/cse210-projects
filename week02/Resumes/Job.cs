ec9431418cd3e8f255b15e49005c546b842aa68b
Paper-Dad <broberts@byupathway.edu>
2025-09-08 20:11:54 -0600

    Started on Job File

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,7 @@
+public class Job
+{
+    public string _compnay;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._compnay = "Microsoft";
+        job1._jobTitle = "Programmer";
+        job1._startYear = 2023;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Programmer";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Job.display();
     }
 }
