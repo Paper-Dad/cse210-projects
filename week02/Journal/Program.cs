f3cc99a05ab86673ba45975b85522b778e4ecb66
Paper-Dad <broberts@byupathway.edu>
2025-09-11 22:42:11 -0600

    Completed Journal Assignment

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        PromptGenerator prompt = new PromptGenerator();
+        Journal journal = new Journal();
+
+        int userOption = 0;
+        while (userOption != 5)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("What would you like to do?");
+
+            string response = Console.ReadLine();
+            userOption = int.Parse(response);
+
+            if (userOption <= 0)
+            {
+                Console.WriteLine("Please enter a valid option.");
+            }
+            else if (userOption >= 6)
+            {
+                Console.WriteLine("Please enter a valid option.");
+            }
+            else if (userOption == 1)
+            {
+                Entry newEntry = new Entry();
+                DateTime theCurrentTime = DateTime.Now;
+                string dateText = theCurrentTime.ToShortDateString();
+                newEntry._date = dateText;
+                newEntry._promptText = prompt.GetRandomPrompt();
+                Console.WriteLine(newEntry._promptText);
+                Console.Write("> ");
+                newEntry._entryText = Console.ReadLine();
+
+                journal.AddEntry(newEntry);
+            }
+            else if (userOption == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (userOption == 3)
+            {
+                Console.WriteLine("What is the filename?");
+                Console.Write("> ");
+                journal.LoadFromFile(Console.ReadLine());
+            }
+            else if (userOption == 4)
+            {
+                Console.WriteLine("What is the filename?");
+                Console.Write("> ");
+                journal.SaveToFile(Console.ReadLine());
+            }
+        }
     }
 }
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,9 +1,20 @@
 public class PromptGenerator
 {
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string>();
+    private Random _random = new Random();
+
+    public PromptGenerator()
+    {
+        _prompts.Add("What was your biggest success today?");
+        _prompts.Add("What was an obstacle you needed to overcome today?");
+        _prompts.Add("What is something that you are grateful for today?");
+        _prompts.Add("What is something you learned today?");
+        _prompts.Add("What is something that happened today that didn't happen yesterday?");
+    }
 
     public string GetRandomPrompt()
     {
-        return "";
+        int index = _random.Next(_prompts.Count);
+        return _prompts[index];
     }
 }
